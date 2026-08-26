183f558f59f4db8a988f2595d11ddfc4d002eece
David Ngungila <davidngungila@gmail.com>
2026-08-26 11:04:04 +0300

    Use logo as site favicon and apple-touch icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    {{-- Favicon --}}
+    <link rel="icon" type="image/png" href="/lau-adventuress-logo.png">
+    <link rel="apple-touch-icon" href="/lau-adventuress-logo.png">
+
     <title>@yield('page-title', 'Tanzania Safari Tours, Kilimanjaro & Zanzibar | LAU Paradise Adventure')</title>
 
     {{-- Google Analytics --}}
